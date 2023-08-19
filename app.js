4de30da4bc87229f3581a9ea82c6ab128e668fde
farukakyol01 <faruk.akyol_arde@demirtag.de>
2023-08-19 12:56:49 +0300

    Primitive type is checked.

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 console.log("Your code goes here...");
 console.log("It is formatted...");
 console.log("Server is running...");
+console.log("Server is running...");
+function add(n1, n2) {
+    //  if (typeof n1 !== "number" || typeof n2 !== "number") {
+    //    throw Error("Incorrect input!");
+    // }
+    console.log(typeof number1);
+    return n1 + n2;
+}
+var number1 = 5;
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -2,3 +2,18 @@ console.log("Your code goes here...");
 console.log("It is formatted...");
 console.log("Server is running...");
 console.log("Server is running...");
+
+function add(n1: number, n2: number) {
+  //  if (typeof n1 !== "number" || typeof n2 !== "number") {
+  //    throw Error("Incorrect input!");
+  // }
+
+  console.log(typeof number1);
+  return n1 + n2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
